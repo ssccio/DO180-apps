989510d6e8ed5f24811a678059e9d62588a63121
Student User <student@workstation.lab.example.com>
2021-03-18 15:45:39 -0400

    add labs

--- labs/multicontainer-design/deploy/nodejs/networked/run.sh
+++ labs/multicontainer-design/deploy/nodejs/networked/run.sh
@@ -13,7 +13,15 @@ sudo chown -R 27:27 work
 # TODO Add podman run for mysql image here
 # Assign the container an IP from range defined the RFC 1918: 10.88.0.0/16
 
+sudo podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
+-e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 \
+-v $PWD/work/data:/var/lib/mysql/data \
+-v $PWD/work/init:/var/lib/mysql/init \
+--ip 10.88.100.101 do180/mysql-57-rhel7
 
 sleep 9
 
 # TODO Add podman run for todonodejs image here
+sudo podman run -d --name todoapi -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
+-e MYSQL_PASSWORD=mypa55 -p 30080:30080 \
+do180/todonodejs
